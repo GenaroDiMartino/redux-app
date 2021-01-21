1945e6df879d743f55a5e4624517af3971b9e9d9
Genaro Di Martino <gdevkingos@gmail.com>
2021-01-20 22:54:02 -0300

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import * as actions from './contador/contador.actions';
+import { AppState } from './app.reducers';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'redux-app';
+
+  contador: number;
+
+  constructor(private _store: Store<AppState>){
+    this._store.select('contador').subscribe(contador => this.contador = contador);
+  }
+
+  incrementar() {
+    this._store.dispatch(actions.incrementar());
+  }
+
+  decrementar() {
+    this._store.dispatch(actions.decrementar());
+  }
+
 }
